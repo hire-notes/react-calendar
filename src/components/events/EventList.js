a639d6f9c1df5f834527091926efd7d054910bc1
Alex Barstow <alexander.barstow@gmail.com>
2017-08-06 14:18:05 -0400

    added comments to EventList

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {Event} from './Event';
 
 export const EventList = (props) => {
+  
+  // if the API call returns no results, just return an empty string
   if (!props.events) {
     return ''
   }
 
+  // generate a different array of Events depending on the src
   let events = props.events[props.src].map((event) => {
     return (
       <Event
