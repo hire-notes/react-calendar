2a78f97b6eb926a27d7b2be8e0343067bcbd96da
Alex Barstow <alexander.barstow@gmail.com>
2017-08-06 14:33:25 -0400

    more style adjustments

--- src/services/eventsService.js
+++ src/services/eventsService.js
@@ -1,9 +1,9 @@
 const eventsPath = '/api';
 
-export const getEvents = (lat,lng,time, source) => {
-	return fetch(`${ eventsPath }/${ source }
-		?lat=${ lat }
-		&lng=${ lng }
+export const getEvents = (lat, lng, time, source) => {
+	return fetch(`${eventsPath}/${source}
+		?lat=${lat}
+		&lng=${lng}
 		&distance=100
 		&sort=time
 		&since=${Math.round(time.getTime()/1000 - 60*60*24*3)}
